04ac7984bf452188b87b8aad965de333aea8b321
sabrinapenad <sabrinapenad@gmail.com>
2020-05-26 11:11:27 -0300

    Add messages from UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
+
   def current_user
-    @current_user || User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
@@ -10,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
     if !logged_in?
-      flash[:alert] = "You must be logged in to perform that action"
+      flash[:error] = "You must be logged in to perform that action"
       redirect_to login_path
     end
   end
